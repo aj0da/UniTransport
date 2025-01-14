6dba766bcfcabf8b5c904e0dd341ec3cb3dd9d58
aj0da <abdulrahmangoda27@gmail.com>
2025-01-14 03:38:59 +0200

    Create SeddData helper

---
+++ UniTransport.PLL/Helpers/SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UniTransport.DAL.Entities;
+
+namespace UniTransport.PLL.Helpers
+{
+    public static class SeedData
+    {
+        public static async Task InitializeAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+
+            // 1. Seed Roles
+            var roles = new[] { "Admin", "Student" };
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            // 2. Seed Admin User
+            var adminEmail = "admin@unitransport.com";
+            if (await userManager.FindByEmailAsync(adminEmail) == null)
+            {
+                var adminUser = new User
+                {
+                    UserName = "admin",
+                    Email = adminEmail,
+                    FirstName = "System",
+                    LastName = "Admin",
+                    EmailConfirmed = true
+                };
+
+                var result = await userManager.CreateAsync(adminUser, "Admin@12345");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
+        }
+    }
+}
